64fde9590f73adb6c3cb6b2138c4e25990aea915
shivdasbhande05 <shivdasbhande@gmail.com>
2025-01-27 10:32:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 //your JS code here. If required.
+const error = document.getElementById("error");
+const loading = document.getElementById("loading");
 const output = document.getElementById("output");
 const btn = document.getElementById("download-images-button");
 
@@ -7,3 +9,26 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+function loadImage(image) {
+  return new Promise((resolve,reject) => {
+    const img = new Image();
+    img.src = image.url;
+    img.onload = resolve(img);
+    img.onerror = reject(new Error(`Failed to load image's URL: ${image.url}`))
+  })
+}
+
+function allPromises() {
+  Promise.all(images.map(loadImage)).then((imageElements)=>{
+    imageElements.forEach(img => {
+      output.appendChild(img);
+    })
+  }).catch((err) => {
+    error.innerText = err.messege;
+  });
+}
+
+btn.addEventListener("click" , allPromises);
+
+
